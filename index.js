f6e600a85b5a78fb3321bf2624b1b82b80205a02
Gabeif7 <105760989+Gabeif7@users.noreply.github.com>
2023-05-18 01:32:51 -0400

    added additional items

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ document.body.append(image);
 newImage('assets/green-character.gif', '100px', '100px');
 newImage('assets/pine-tree.png', '450px', '200px');
 
+newImage('assets/tree.png', '200px', '300px')
+newImage('assets/pillar.png', '350px', '100px')
+newImage('assets/crate.png', '150px', '200px')
+newImage('assets/well.png', '500px', '425px')
+
 
 
 
