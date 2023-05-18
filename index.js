18c9fc421cae4304e28798f78f3f05985f9139db
Gabeif7 <105760989+Gabeif7@users.noreply.github.com>
2023-05-18 01:55:06 -0400

    added all additional items and chnaged event listener to dblclick

--- index.js
+++ index.js
@@ -7,13 +7,31 @@ image.style.bottom = bottom;
 document.body.append(image);
 }
 
+function newItem(src, left, bottom) {
+let item = document.createElement('img');
+item.src = src;
+item.style.position = 'fixed';
+item.style.left = left;
+item.style.bottom = bottom;
+document.body.append(item);
+
+item.addEventListener('dblclick', function() {
+    item.remove();
+});
+}
+
+newItem('assets/sword.png', '500px', '405px')
+newItem('assets/shield.png', '165px', '185px')
+newItem('assets/staff.png', '600px', '100px')
+
 newImage('assets/green-character.gif', '100px', '100px');
 newImage('assets/pine-tree.png', '450px', '200px');
-
 newImage('assets/tree.png', '200px', '300px')
 newImage('assets/pillar.png', '350px', '100px')
 newImage('assets/crate.png', '150px', '200px')
 newImage('assets/well.png', '500px', '425px')
+
+/*
 
 let sword = document.createElement('img');
 sword.src = 'assets/sword.png';
@@ -26,6 +44,7 @@ sword.addEventListener('click', function() {
     sword.remove();
 });
 
+*/
 
 
 /*
