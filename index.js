08d54449545ed0ee6d99c23ccea352febef826d1
Gabeif7 <105760989+Gabeif7@users.noreply.github.com>
2023-05-18 01:40:39 -0400

    I needed to add style in for sword to be located on page where it was suppossed to be, code given in instructions didn't work

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ newImage('assets/pillar.png', '350px', '100px')
 newImage('assets/crate.png', '150px', '200px')
 newImage('assets/well.png', '500px', '425px')
 
+let sword = document.createElement('img');
+sword.src = 'assets/sword.png';
+sword.style.position = 'fixed';
+sword.style.left = '500px';
+sword.style.bottom = '405px';
+document.body.append(sword);
+
+sword.addEventListener('click', function() {
+    sword.remove();
+});
 
 
 
