3dd4ece338a5f8f39f2d61a1cc33fb0ec9a4f07e
Gabeif7 <105760989+Gabeif7@users.noreply.github.com>
2023-05-18 01:27:33 -0400

    finally figured out I mistyped 1 letter in my function. invoked newImage with parameters for pinetree and character

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+function newImage(src, left, bottom) {
+let image = document.createElement('img');
+image.src = src;
+image.style.position = 'fixed';
+image.style.left = left;
+image.style.bottom = bottom;
+document.body.append(image);
+}
+
+newImage('assets/green-character.gif', '100px', '100px');
+newImage('assets/pine-tree.png', '450px', '200px');
+
+
+
+
+/*
 let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
@@ -11,3 +27,4 @@ pineTree.style.position = 'fixed'
 pineTree.style.left = '450px'
 pineTree.style.bottom = '200px'
 document.body.append(pineTree)
+*/
